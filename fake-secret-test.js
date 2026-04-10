3064030c5934310240e8bbf09db7c5517e1ec25e
benie-joy-possi <beniejoypossi@gmail.com>
2026-04-10 15:02:27 +0100

    test: trigger gitleaks pipeline failure.

---
+++ fake-secret-test.js
@@ -0,0 +1,6 @@
+// This file is intentionally placed to test the Gitleaks GitHub Action.
+// It contains a fake, non-functional AWS access credential.
+// DO NOT use real credentials here.
+
+const AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EKSA423";
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
